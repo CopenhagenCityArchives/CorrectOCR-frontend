fcd30a3e091fc430386ad51a481fc3929636cb4f
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2020-01-10 11:47:40 +0100

    DB init script: fix colum names to refer to docs instead of files

--- initdb.d/dbinit.sql
+++ initdb.d/dbinit.sql
@@ -1,7 +1,7 @@
 CREATE TABLE token (
 	kind VARCHAR(255) NOT NULL,
-	file_id VARCHAR(255) NOT NULL,
-	file_index INT NOT NULL,
+	doc_id VARCHAR(255) NOT NULL,
+	doc_index INT NOT NULL,
 	original VARCHAR(255) NOT NULL,
 	gold VARCHAR(255),
 	bin INT,
@@ -10,25 +10,24 @@ CREATE TABLE token (
 	selection VARCHAR(255),
 	token_type VARCHAR(255),
 	token_info TEXT,
-	PRIMARY KEY (file_id, file_index)
+	PRIMARY KEY (doc_id, doc_index)
 );
 
 CREATE TABLE kbest (
-	id INT NOT NULL AUTO_INCREMENT,
-	file_id VARCHAR(255) NOT NULL,
-	file_index INT NOT NULL,
+	id PRIMARY KEY,
+	doc_id VARCHAR(255) NOT NULL,
+	doc_index INT NOT NULL,
 	k INT NOT NULL,
 	candidate VARCHAR(255) NOT NULL,
-	probability float NOT NULL,
-	PRIMARY KEY (id),
-	CONSTRAINT FOREIGN KEY fk_kbest_token(file_id, file_index) REFERENCES token(file_id, file_index)
+	probability float NOT NULL --,
+--	FOREIGN KEY fk_kbest_token(doc_id, doc_index) REFERENCES token(doc_id, doc_index)
 );
 
-CREATE INDEX idx_token_file_id_file_index
-	ON token(file_id, file_index);
+CREATE INDEX idx_token_doc_id_doc_index
+	ON token(doc_id, doc_index);
 
-CREATE INDEX idx_token_file_id_kind
-	ON token(file_id, kind);
+CREATE INDEX idx_token_doc_id_kind
+	ON token(doc_id, kind);
 
-CREATE INDEX idx_kbest_file_id_file_index
-	ON token(file_id, file_index);
+CREATE INDEX idx_kbest_doc_id_doc_index
+	ON token(doc_id, doc_index);
