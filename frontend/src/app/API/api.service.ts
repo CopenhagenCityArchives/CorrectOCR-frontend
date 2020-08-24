ddce5dccf2beb71b76cd6f6ce76f38f3d6c3dcca
Jonsabban <Sanox3@hotmail.dk>
2020-08-24 14:37:06 +0200

    API-sevice: Added documentation

--- frontend/src/app/API/api.service.ts
+++ frontend/src/app/API/api.service.ts
@@ -24,35 +24,85 @@ export class ApiService {
   public getOverview() {
     return this.http.get(url);
   }
-
-  public getToken(token): Observable<Object> {
-    return this.http.get(url + token);
+  
+  /**
+   * Get a specific token
+   *
+   * @param   {string}              docid  id of document
+   * @param   {number<Object>}      index  index of token
+   *
+   * @return  {Observable<Object>}         [return Observable]
+   */
+  public getToken(docid:string, index:number): Observable<Object> {
+    return this.http.get(url + docid + '/token-' + index + '.json');;
   }
 
-  public getTokensFromDocId(doc_id) {
-    return this.http.get(url + doc_id + '/tokens.json');
+  /**
+   * Get all tokens from given document id
+   *
+   * @param   {string}  docid  id of document
+   *
+   * @return  {Observable<Object>}         [return Observable]
+   */
+  public getTokensFromDocId(docid:string): Observable<Object> {
+    return this.http.get(url + docid + '/tokens.json');
   }
 
-  public getRandomToken() {
+  /**
+   * Get a random token from a random document
+   *
+   * @return  {Observable<Object>}         [return Observable]
+   */
+  public getRandomToken(): Observable<Object> {
     return this.http.get(url + 'random', this.options);
   }
 
-  public getLeftToken(mainToken: IToken) {
+  /**
+   * Get token with index-1 of given token
+   *
+   * @param   {IToken<Object>}      mainToken  Main Token
+   *
+   * @return  {Observable<Object>}             [return Observable]
+   */
+  public getLeftToken(mainToken: IToken): Observable<Object> {
     return this.http.get(url + mainToken.doc_ID + '/token-' + (mainToken.index - 1) + '.json', this.options);
   }
 
-  public getRightToken(mainToken: IToken) {
+  /**
+   * Get token with index+1 of given token
+   *
+   * @param   {IToken<Object>}      mainToken  Main Token
+   *
+   * @return  {Observable<Object>}             [return Observable]
+   */
+  public getRightToken(mainToken: IToken): Observable<Object> {
     return this.http.get(url + mainToken.doc_ID + '/token-' + (mainToken.index + 1) + '.json', this.options);
   }
 
-  public postHypernate(mainToken: IToken, hypDir: string) {
+  /**
+   * Hypernate main token in givne direction
+   *
+   * @param   {IToken}              mainToken  Main Token
+   * @param   {string<Object>}      hypDir     hypernate direction, either left or right
+   *
+   * @return  {Observable<Object>}             [return Observable]
+   */
+  public postHypernate(mainToken: IToken, hypDir: string): Observable<Object> {
     let body = {
       'hyphenate': hypDir
     };
     return this.http.request('POST', (url + mainToken.doc_ID + '/token-' + (mainToken.index) + '.json'), {body: body});
   }
 
-  public postGold(mainToken: IToken, gold: string) {
+  /**
+   * Update token with new gold value
+   *
+   * @param   {IToken}              mainToken  Main Token
+   * @param   {string<Object>}      gold       new gold value
+   *
+   * @return  {Observable<Object>}             [return Observable]
+   */
+  public postGold(mainToken: IToken, gold: string): Observable<Object> {
     let body = {
       'gold': gold
     };
