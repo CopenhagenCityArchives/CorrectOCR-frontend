018430dd13139b016b5f0e3e0951766652cf2566
Jonsabban <Sanox3@hotmail.dk>
2020-08-24 10:46:16 +0200

    Added doc-overview component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TokensComponent } from './tokens/tokens.component';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent } from './app.component';
+import { TokensComponent } from './tokens/tokens.component';
+import { DocOverviewComponent } from './doc-overview/doc-overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TokensComponent,
+    DocOverviewComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,6 +25,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     RouterModule.forRoot([
       {path: '', component: AppComponent},
       {path: 'tokens', component: TokensComponent},
+      {path: 'doc-overview', component: DocOverviewComponent},
     ]),
     NgbModule
   ],
