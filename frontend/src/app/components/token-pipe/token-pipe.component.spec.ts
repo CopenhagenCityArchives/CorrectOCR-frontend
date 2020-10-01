8c36ca077fe40707458259229c4f159d0c1b9855
Jonsabban <Sanox3@hotmail.dk>
2020-10-01 09:31:52 +0200

    Token pipe fixes

--- frontend/src/app/components/token-pipe/token-pipe.component.spec.ts
+++ frontend/src/app/components/token-pipe/token-pipe.component.spec.ts
@@ -1,15 +1,17 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TokensPipePipe } from 'src/app/custom-pipes/tokens-pipe.pipe';
+import { Token } from '../tokens/token';
 import { TokenPipeComponent } from './token-pipe.component';
 
 describe('TokenPipeComponent', () => {
   let component: TokenPipeComponent;
   let fixture: ComponentFixture<TokenPipeComponent>;
+  let pipe: TokensPipePipe;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TokenPipeComponent ],
+      declarations: [ TokenPipeComponent, TokensPipePipe ],
       imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
@@ -18,10 +20,24 @@ describe('TokenPipeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TokenPipeComponent);
     component = fixture.componentInstance;
+    pipe = new TokensPipePipe();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should return null if provided with no value', () => {
+    let testData:JSON;
+
+    expect(pipe.transform(testData)).toBe(null);
+  });
+
+  it('should return a valid token when given valid JSON data', () => {
+    const testJSON = require('../../../test-helpers/testMainToken.json');
+    expect(pipe.transform(testJSON)).toEqual(new Token(testJSON))
+  });
+  
+
 });
