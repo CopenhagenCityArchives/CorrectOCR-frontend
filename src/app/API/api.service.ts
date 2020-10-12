8d54eaaa790123d9359f15b3d5c1e66af761dfb0
Jonsabban <Sanox3@hotmail.dk>
2020-10-12 12:44:21 +0200

    ApiService: Added basic errorhandling

--- src/app/API/api.service.ts
+++ src/app/API/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { IToken } from '../components/tokens/i-token';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { environment } from '../../environments/environment'
 
@@ -19,6 +19,22 @@ export class ApiService {
 
   constructor(http: HttpClient) {
     this.http = http;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
   }
 
   /**
@@ -41,12 +57,14 @@ export class ApiService {
   public getToken(docid:number, index:number): Observable<Object> {
     return this.http.get(this.url + docid + '/token-' + index + '.json').pipe(
       retry(3),
+      catchError(this.handleError)
     );
   }
 
   public getTokenFromInfoUrl(infoUrl:string): Observable<Object> {
     return this.http.get(this.url + infoUrl).pipe(
       retry(3),
+      catchError(this.handleError)
     );
   }
 
@@ -57,62 +75,12 @@ export class ApiService {
    *
    * @return  {Observable<Object>}         [return Observable]
    */
-  /*public getAllTokensFromDocId(docid:string): Observable<Object> {
-    let doc_6000: Array<Object> = [
-      {"image_url": "/6000/token-6.png",  "info_url": "/6148/token-6.json",  "is_corrected": false, "string": "La&s"},
-      {"image_url": "/6000/token-7.png",  "info_url": "/6148/token-7.json",  "is_corrected": false, "string": "stter."},
-      {"image_url": "/6000/token-10.png", "info_url": "/6148/token-10.json", "is_corrected": false, "string": "anskrør,"},
-      {"image_url": "/6000/token-27.png", "info_url": "/6148/token-27.json", "is_corrected": false, "string": "lalet"},
-      {"image_url": "/6000/token-30.png", "info_url": "/6148/token-30.json", "is_corrected": false, "string": "T^e."},
-      {"image_url": "/6000/token-42.png", "info_url": "/6148/token-42.json", "is_corrected": false, "string": "idten."},
-      {"image_url": "/6000/token-45.png", "info_url": "/6148/token-45.json", "is_corrected": false, "string": ":asse,"},
-      {"image_url": "/6000/token-47.png", "info_url": "/6148/token-47.json", "is_corrected": false, "string": "raed"},
-      {"image_url": "/6000/token-54.png", "info_url": "/6148/token-54.json", "is_corrected": false, "string": "lybninger."},
-      {"image_url": "/6000/token-58.png", "info_url": "/6148/token-58.json", "is_corrected": false, "string": "paalimet"}
-    ]
-    let doc_6148: Array<Object> = [
-      {"image_url": "/6148/token-62.png",  "info_url": "/6148/token-62.json",  "is_corrected": false, "string": "6,"},
-      {"image_url": "/6148/token-71.png",  "info_url": "/6148/token-71.json",  "is_corrected": false, "string": "emmelig"},
-      {"image_url": "/6148/token-73.png",  "info_url": "/6148/token-73.json",  "is_corrected": false, "string": "'09)."},
-      {"image_url": "/6148/token-82.png",  "info_url": "/6148/token-82.json",  "is_corrected": false, "string": "«*|„"},
-      {"image_url": "/6148/token-84.png",  "info_url": "/6148/token-84.json",  "is_corrected": false, "string": "6,"},
-      {"image_url": "/6148/token-94.png",  "info_url": "/6148/token-94.json",  "is_corrected": false, "string": "Politløvrlgheder."},
-      {"image_url": "/6148/token-100.png", "info_url": "/6148/token-100.json", "is_corrected": false, "string": "Murersvendene:"},
-      {"image_url": "/6148/token-105.png", "info_url": "/6148/token-105.json", "is_corrected": false, "string": "Nepken,"},
-      {"image_url": "/6148/token-137.png", "info_url": "/6148/token-137.json", "is_corrected": false, "string": "Gummerup,"},
-      {"image_url": "/6148/token-167.png", "info_url": "/6148/token-167.json", "is_corrected": false, "string": "giverenke"}
-    ]
-    let doc_6500: Array<Object> = [
-      {"image_url": "/6148/token-231.png", "info_url": "/6148/token-231.json", "is_corrected": false, "string": "Efternævnle"},
-      {"image_url": "/6148/token-247.png", "info_url": "/6148/token-247.json", "is_corrected": false, "string": "Draaby"},
-      {"image_url": "/6148/token-259.png", "info_url": "/6148/token-259.json", "is_corrected": false, "string": "hjemme­"},
-      {"image_url": "/6148/token-265.png", "info_url": "/6148/token-265.json", "is_corrected": false, "string": "Lnursdotter"},
-      {"image_url": "/6148/token-271.png", "info_url": "/6148/token-271.json", "is_corrected": false, "string": "hebruar"},
-      {"image_url": "/6148/token-274.png", "info_url": "/6148/token-274.json", "is_corrected": false, "string": "Stabrand,"},
-      {"image_url": "/6148/token-275.png", "info_url": "/6148/token-275.json", "is_corrected": false, "string": "Nødager"},
-      {"image_url": "/6148/token-287.png", "info_url": "/6148/token-287.json", "is_corrected": false, "string": "Kjeldstrup"},
-      {"image_url": "/6148/token-289.png", "info_url": "/6148/token-289.json", "is_corrected": false, "string": "tiggaard"},
-      {"image_url": "/6148/token-295.png", "info_url": "/6148/token-295.json", "is_corrected": false, "string": "Politi­"}
-    ]
-
-    switch (docid) {
-      case "6000":
-        return of(doc_6000);
-        break;
-      
-      case "6148":
-        return of(doc_6148);
-        break;
-
-      case "6500":
-        return of(doc_6500)
-        break;
-    }
-
-  }*/
   
   public getAllTokensFromDocId(docid:string): Observable<Object> {
-   return this.http.get(this.url + docid + '/tokens.json');
+   return this.http.get(this.url + docid + '/tokens.json').pipe(
+     retry(3),
+     catchError(this.handleError)
+   );
   }
 
 
@@ -122,11 +90,10 @@ export class ApiService {
    * @return  {Observable<Object>}         [return Observable]
    */
   public getRandomToken(): Observable<Object> {
-    return this.http.get(this.url + 'random');
-  }
-
-  public getTestToken(): Observable<Object> {
-    return this.http.get('http://localhost:5000/6000/token-153.json');
+    return this.http.get(this.url + 'random').pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
   }
 
   /**
@@ -139,6 +106,7 @@ export class ApiService {
   public getLeftToken(mainToken: IToken): Observable<Object> {
     return this.http.get(this.url + mainToken.doc_ID + '/token-' + (mainToken.index - 1) + '.json').pipe(
       retry(3),
+      catchError(this.handleError)
     );
   }
 
@@ -152,6 +120,7 @@ export class ApiService {
   public getRightToken(mainToken: IToken): Observable<Object> {
     return this.http.get(this.url + mainToken.doc_ID + '/token-' + (mainToken.index + 1) + '.json').pipe(
       retry(3),
+      catchError(this.handleError)
     );
   }
 
@@ -164,10 +133,10 @@ export class ApiService {
    * @return  {Observable<Object>}             [return Observable]
    */
   public postHypernate(mainToken: IToken, hypDir: string): Observable<Object> {
-    let body = {
-      'hyphenate': hypDir
-    };
-    return this.http.request('POST', (this.url + mainToken.doc_ID + '/token-' + (mainToken.index) + '.json'), {body: body});
+    let body = { 'hyphenate': hypDir };
+    return this.http.request('POST', (this.url + mainToken.doc_ID + '/token-' + (mainToken.index) + '.json'), {body: body}).pipe(
+      catchError(this.handleError)
+    );
   }
 
   /**
@@ -179,10 +148,10 @@ export class ApiService {
    * @return  {Observable<Object>}             [return Observable]
    */
   public postGold(mainToken: IToken, gold: string): Observable<Object> {
-    let body = {
-      'gold': gold
-    };
-    return this.http.request('POST',(this.url +  mainToken.doc_ID + '/token-' + (mainToken.index) + '.json'), {body: body});
+    let body = { 'gold': gold };
+    return this.http.request('POST',(this.url +  mainToken.doc_ID + '/token-' + (mainToken.index) + '.json'), {body: body}).pipe(
+      catchError(this.handleError)
+    );
   }
   
 }
