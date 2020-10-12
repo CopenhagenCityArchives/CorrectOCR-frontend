493b38bcd80560d252918da9e97315730dd67a8e
Jonsabban <Sanox3@hotmail.dk>
2020-10-12 12:46:35 +0200

    PipeComponents: Test suite updated with more tests

--- src/app/custom-pipes/tokens-pipe.pipe.spec.ts
+++ src/app/custom-pipes/tokens-pipe.pipe.spec.ts
@@ -1,8 +1,27 @@
+import { Token } from '../components/tokens/token';
 import { TokensPipePipe } from './tokens-pipe.pipe';
 
 describe('TokensPipePipe', () => {
+  let pipe: TokensPipePipe;
+
+  beforeEach(() => {
+    pipe = new TokensPipePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new TokensPipePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should return null if provided with no value', () => {
+    let testData:JSON;
+
+    expect(pipe.transform(testData)).toBe(null);
+  });
+
+  it('should return a valid token when given valid JSON data', () => {
+    const testJSON = require('../../test-helpers/testMainToken.json');
+    expect(pipe.transform(testJSON)).toEqual(new Token(testJSON))
+  });
+
+
 });
