3edccc391d9461217bd6fbea63ce37d2e7137389
Bo Henriksen <stumpdenmark@hotmail.com>
2020-10-22 15:19:23 +0200

    Update environment.prod.ts
    Change callback url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  auth0Redirect: "https://static.kbharkiv.dk/correctocr/",
+  auth0Redirect: "https://static.kbharkiv.dk/correctocr",
   //auth0Redirect: `${window.location.origin}`,
   apiUrl: "https://correctocr-api.kbharkiv.dk/",
   documentBaseUrl: "https://api.kbharkiv.dk/asset/"
