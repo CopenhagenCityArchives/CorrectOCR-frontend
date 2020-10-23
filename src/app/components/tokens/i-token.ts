a2088ef3e9606228a4f971229e15b85f8e9d86f8
Jonsabban <Sanox3@hotmail.dk>
2020-10-23 13:13:27 +0200

    Update i-token.ts

--- src/app/components/tokens/i-token.ts
+++ src/app/components/tokens/i-token.ts
@@ -1,23 +1,24 @@
 export interface IToken {
-    firstBest:  string,
-    firstProb:  number,
-    secondBest: string,
-    secondProb: number,
-    thirdBest:  string,
-    thirdProb:  number,
-    fourthBest: string,
-    fourthProb: number,
-    bin:        number,
-    decision:   string,
-    doc_ID:     number,
-    gold:       string,
-    heuristic:  string,
-    hyphenated: string,
-    index:      number,
-    original:   string,
-    selection:  string[],
-    token_info: string[],
-    token_type: string,
-    image_url:  string
-
+    firstBest: string;
+    firstProb: number;
+    secondBest: string;
+    secondProb: number;
+    thirdBest: string;
+    thirdProb: number;
+    fourthBest: string;
+    fourthProb: number;
+    bin: number;
+    decision: string;
+    doc_ID: number;
+    frame: number[];
+    gold: string;
+    heuristic: string;
+    hyphenated: string;
+    image_url: string;
+    index: number;
+    original: string;
+    page: number;
+    selection: string[];
+    token_info: string[];
+    token_type: string;
 }
