76ceaa44eda3687f549adad1d8cb7ea02104938c
Jonsabban <Sanox3@hotmail.dk>
2020-10-23 10:53:21 +0200

    Update Token with Page & Frame prob

--- src/app/components/tokens/token.ts
+++ src/app/components/tokens/token.ts
@@ -12,15 +12,17 @@ export class Token implements IToken {
     bin: number;
     decision: string;
     doc_ID: number;
+    frame: number[];
     gold: string;
     heuristic: string;
     hyphenated: string;
+    image_url: string;
     index: number;
     original: string;
+    page: number;
     selection: string[];
     token_info: string[];
     token_type: string;
-    image_url: string;
 
     constructor(json: JSON) {
         this.firstBest  = json['1-best'];
@@ -34,15 +36,17 @@ export class Token implements IToken {
         this.bin        = json['Bin'];
         this.decision   = json['Decision'];
         this.doc_ID     = json['Doc ID'];
+        this.frame      = json['Frame'];
         this.gold       = json['Gold'];
         this.heuristic  = json['Heuristic'];
         this.hyphenated = json['Hyphenated'];
+        this.image_url  = json['image_url'];
         this.index      = json['Index'];
         this.original   = json['Original'];
+        this.page       = json['Page']
         this.selection  = json['Selection'];
         this.token_info = json['Token info'];
         this.token_type = json['Token type'];
-        this.image_url  = json['image_url'];
     };
 
 }
