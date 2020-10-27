40ea77caacdf3adef1f341a494997045ef36696f
Bo Henriksen <stumpdenmark@hotmail.com>
2020-10-27 12:53:33 +0100

    Update environment.prod.ts
    Add trailing slash to redirect_uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  auth0Redirect: "https://static.kbharkiv.dk/correctocr",
-  //auth0Redirect: `${window.location.origin}`,
+  auth0Redirect: "https://static.kbharkiv.dk/correctocr/",
   apiUrl: "https://correctocr-api.kbharkiv.dk/",
   documentBaseUrl: "https://api.kbharkiv.dk/asset/"
 };
