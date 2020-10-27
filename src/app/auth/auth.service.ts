7b5d9b7f1bef374709ad0770bf38ec604a376ca6
Bo Henriksen <stumpdenmark@hotmail.com>
2020-10-27 11:32:35 +0100

    Update auth.service.ts
    Dont override redirect_uri when logging in

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -79,7 +79,6 @@ export class AuthService {
     this.auth0Client$.subscribe((client: Auth0Client) => {
       // Call method to log in
       client.loginWithRedirect({
-        redirect_uri: `${window.location.origin}`,
         appState: { target: redirectPath }
       });
     });
