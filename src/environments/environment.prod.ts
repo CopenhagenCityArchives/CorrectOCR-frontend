6438c7de5751ced6018b9f66590862e2e4567bf7
Bo Henriksen <stumpdenmark@hotmail.com>
2020-10-29 13:01:47 +0100

    Update environment.prod.ts
    Change redirect uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  auth0Redirect: "https://static.kbharkiv.dk/correctocr/",
+  auth0Redirect: "https://correctocr.kbharkiv.dk/",
   apiUrl: "https://correctocr-api.kbharkiv.dk/",
   documentBaseUrl: "https://api.kbharkiv.dk/asset/"
 };
